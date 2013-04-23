f8be6fff027767603c70a566f6591588caf86183
bingo <blackbing@gmail.com>
2013-04-23 18:16:24 +0800

    release 0.0.2

--- release/WorkerD.js
+++ release/WorkerD.js
@@ -1,21 +1,28 @@
 
-define('text!worker_console.js',[],function () { return '(function() {\n  var attr, console_attr, _i, _len,\n    _this = this;\n\n  this.postDebugMessage = function(type, args) {\n    return postMessage({\n      debug: type,\n      args: args\n    });\n  };\n\n  console_attr = [\'debug\', \'error\', \'info\', \'log\', \'warn\', \'dir\', \'dirxml\', \'trace\', \'assert\', \'count\', \'markTimeline\', \'profile\', \'profileEnd\', \'time\', \'timeEnd\', \'timeStamp\', \'group\', \'groupCollapsed\', \'groupEnd\'];\n\n  this.console = {};\n\n  for (_i = 0, _len = console_attr.length; _i < _len; _i++) {\n    attr = console_attr[_i];\n    this.console[attr] = (function(attr) {\n      return function() {\n        return _this.postDebugMessage(attr, arguments);\n      };\n    })(attr);\n  }\n\n}).call(this);\n';});
+define('text!worker_console.js',[],function () { return '(function() {\n  var attr, console_attr, _i, _len,\n    _this = this;\n\n  this.postDebugMessage = function(type, args) {\n    return self.postMessage({\n      debug: type,\n      args: args\n    });\n  };\n\n  console_attr = [\'debug\', \'error\', \'info\', \'log\', \'warn\', \'group\', \'groupCollapsed\', \'groupEnd\', \'dir\', \'trace\', \'assert\', \'count\', \'markTimeline\', \'profile\', \'profileEnd\', \'time\', \'timeEnd\', \'timeStamp\'];\n\n  self.console = {};\n\n  for (_i = 0, _len = console_attr.length; _i < _len; _i++) {\n    attr = console_attr[_i];\n    self.console[attr] = (function(attr) {\n      return function() {\n        var args;\n\n        args = Array.prototype.slice.call(arguments);\n        return _this.postDebugMessage(attr, args);\n      };\n    })(attr);\n  }\n\n}).call(this);\n';});
 
 define('text!worker_event.js',[],function () { return '(function() {\n  var Callbacks,\n    _this = this;\n\n  Callbacks = {};\n\n  this.send = function(id, data) {\n    var res;\n\n    res = {};\n    res.msgId = id;\n    res[id] = data;\n    return self.postMessage(res);\n  };\n\n  this.on = function(id, callback) {\n    return Callbacks[id] = callback;\n  };\n\n  this.off = function(id) {\n    return delete Callbacks[id];\n  };\n\n  this.addEventListener("message", function(event) {\n    var data, id, msgData;\n\n    data = event.data;\n    if ((data.msgId != null) && (data.msgData != null)) {\n      id = data.msgId;\n      msgData = data.msgData;\n      if (Callbacks[id]) {\n        return Callbacks[id](msgData);\n      }\n    }\n  });\n\n}).call(this);\n';});
 
 (function() {
   define('worker_util',['require','text!./worker_console.js','text!./worker_event.js'],function(require) {
-    var URL, WorkerUtil, importRequirejs, requirejsPath, worker_console_js, worker_event_js;
+    var WorkerUtil, worker_console_js, worker_event_js;
 
     worker_console_js = require('text!./worker_console.js');
     worker_event_js = require('text!./worker_event.js');
-    requirejsPath = 'http://requirejs.org/docs/release/2.1.5/minified/require.js';
-    importRequirejs = "importScripts('" + requirejsPath + "');";
-    URL = window.URL || window.webkitURL;
     WorkerUtil = (function() {
-      var append_console, consoleStyle, getSharedWorkerURL, options, prepareInlineDebug, storeSharedWorkerURL;
+      var URL, append_console, consoleStyle, consoleStylePrefix, getSharedWorkerURL, importRequirejs, options, prepareInlineDebug, requirejsPath, storeSharedWorkerURL, supportSyntaxList;
 
-      consoleStyle = 'background: #000; color: #FFF;';
+      URL = window.URL || window.webkitURL;
+
+      requirejsPath = 'http://requirejs.org/docs/release/2.1.5/minified/require.js';
+
+      importRequirejs = "importScripts('" + requirejsPath + "');";
+
+      consoleStyle = 'background: #555454; color: #fff; padding: 2px;';
+
+      supportSyntaxList = ['debug', 'error', 'info', 'log', 'warn', 'group', 'groupCollapsed', 'groupEnd'];
+
+      consoleStylePrefix = '%c';
 
       append_console = function(content, opts) {
         var jsContent;
@@ -45,15 +52,17 @@ define('text!worker_event.js',[],function () { return '(function() {\n  var Call
           }
           data = event.data;
           if (typeof data === 'object' && (data.debug != null)) {
-            console.group("%c console from worker", consoleStyle);
             args = [];
             _ref = data.args;
             for (k in _ref) {
               v = _ref[k];
               args.push(v);
             }
-            console[data.debug].apply(console, args);
-            return console.groupEnd("%c console from worker", consoleStyle);
+            if ($.inArray(data.debug, supportSyntaxList) >= 0) {
+              args[0] = consoleStylePrefix + args[0];
+              args.push(consoleStyle);
+            }
+            return console[data.debug].apply(console, args);
           }
         }, false);
         inlineWorker.addEventListener('error', function(event) {
@@ -91,9 +100,9 @@ define('text!worker_event.js',[],function () { return '(function() {\n  var Call
           type: 'application/javascript'
         });
         blobWorker_url = URL.createObjectURL(blobWorker);
+        console.log(blobWorker_url);
         inlineWorker = new Worker(blobWorker_url);
         prepareInlineDebug(inlineWorker, opts);
-        URL.revokeObjectURL(blobWorker_url);
         return inlineWorker;
       };
 
